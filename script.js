d01639e5f9b0d4328d24ea2c5b7d0bcb2ed68bc9
0xJuune <jlbenfield.cccmc@gmail.com>
2022-09-22 19:36:37 -0400

    add function for display hiding

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
-const 
+const ratePlanArea = document.getElementById('dataPlanContainer')
+const formArea = document.getElementById('formArea')
+const loginButton = document.getElementById('logo')
+let hideState = 0
 
-switch (plan) {
-    case 'basic':
-        let;
-        break;
-    case 'power':
-        let;
-        break;
-    case 'unlimited':
-        let;
-        break;
+
+const hideBoxes = () => {
+    console.log('yeehaw')
+    if (!hideState) {
+        ratePlanArea.style.display = 'none'
+        formArea.style.display = 'inline'
+        hideState = 1
+    }
+    else {
+        ratePlanArea.style.display = 'inline'
+        formArea.style.display = 'none'
+        hideState = 0
+    }
 }
+
+loginButton.addEventListener("click", hideBoxes)
+
+// switch (plan) {
+//     case 'basic':
+//         let;
+//         break;
+//     case 'power':
+//         let;
+//         break;
+//     case 'unlimited':
+//         let;
+//         break;
+// }
